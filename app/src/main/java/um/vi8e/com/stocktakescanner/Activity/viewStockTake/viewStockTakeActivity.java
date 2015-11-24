8b2f5e94230691b5d6b9d279cf02c3a189843521
umitems <umitems@gmail.com>
2015-11-24 20:07:57 +0700

    add navigate drawer phase1

--- app/src/main/java/um/vi8e/com/stocktakescanner/Activity/viewStockTake/viewStockTakeActivity.java
+++ app/src/main/java/um/vi8e/com/stocktakescanner/Activity/viewStockTake/viewStockTakeActivity.java
@@ -1,10 +1,14 @@
 package um.vi8e.com.stocktakescanner.Activity.viewStockTake;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import um.vi8e.com.stocktakescanner.Activity.CoreActivity;
 import um.vi8e.com.stocktakescanner.Model.ModelType;
@@ -15,6 +19,7 @@ import um.vi8e.com.stocktakescanner.utils.RecycleUtil;
 
 public class viewStockTakeActivity extends CoreActivity implements ActionBar.TabListener{
 ViewStockFragment viewStockFragment;
+private DrawerLayout mDrawerLayout;
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
@@ -34,9 +39,25 @@ protected void onCreate(Bundle savedInstanceState) {
 	tabLayout.setClickable(false);
 	tabLayout.setEnabled(false);
 
+	mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+	NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+	if (navigationView != null) {
+		setupDrawerContent(navigationView);
+	}
 }
 
+private void setupDrawerContent(NavigationView navigationView) {
+	navigationView.setNavigationItemSelectedListener(
+			new NavigationView.OnNavigationItemSelectedListener() {
+				@Override
+				public boolean onNavigationItemSelected(MenuItem menuItem) {
+					menuItem.setChecked(true);
+					mDrawerLayout.closeDrawers();
+					return true;
+				}
+			});
+}
 
 @Override public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
 
@@ -48,5 +69,15 @@ protected void onCreate(Bundle savedInstanceState) {
 
 @Override public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
+}
+
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+	switch (item.getItemId()) {
+		case android.R.id.home:
+			mDrawerLayout.openDrawer(GravityCompat.START);
+			return true;
+	}
+	return super.onOptionsItemSelected(item);
 }
 }
